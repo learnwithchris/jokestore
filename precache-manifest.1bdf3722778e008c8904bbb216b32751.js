6e87bd12ac0b8a8129dba01a5837a6f44a951c9b
learnwithchris <cegan35@gmail.com>
2020-03-24 12:52:31 -0400

    Updates

---
+++ precache-manifest.1bdf3722778e008c8904bbb216b32751.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "200c3b1add92799e212c1f0c9e46ee12",
+    "url": "/jokestore/index.html"
+  },
+  {
+    "revision": "7c2bc665448843ea2350",
+    "url": "/jokestore/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7c2bc665448843ea2350",
+    "url": "/jokestore/static/js/2.74151a35.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/jokestore/static/js/2.74151a35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58835d4d4fbb62ef2424",
+    "url": "/jokestore/static/js/main.4f0a85ff.chunk.js"
+  },
+  {
+    "revision": "21bbb70423f3a9ea9a86",
+    "url": "/jokestore/static/js/runtime-main.c3610f2c.js"
+  }
+]);
